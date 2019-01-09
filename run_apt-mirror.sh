94ed619568d636ddc01e961bb91f9b01ed9df256
Masaharu TASHIRO <tashiro@leo10.cse.kyutech.ac.jp>
2019-01-10 00:27:19 +0900

    run_apt-mirror.shを修正

--- run_apt-mirror.sh
+++ run_apt-mirror.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-su - apt-mirror -c "/usr/bin/apt-mirror > /var/spool/apt-mirror/var/cron.log"
-su - apt-mirror -c "/var/spool/apt-mirror/var/clean.sh >> /var/spool/apt-mirror/var/cron.log;"
+su - apt-mirror -c "/usr/bin/apt-mirror 2>&1 | tee /var/spool/apt-mirror/var/cron.log"
+su - apt-mirror -c "/var/spool/apt-mirror/var/clean.sh 2>&1 | tee /var/spool/apt-mirror/var/cron.log;"
